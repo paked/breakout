6a87c46c1b2e106c82d2a6b81366d79a3533d327
Harrison Shoebridge <harrison@theshoebridges.com>
2016-05-23 21:39:05 +1000

    Initial commit

---
+++ Assets/scripts/BallController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+    public float startSpeed = 10;
+
+    private float speed;
+    private Rigidbody rb;
+    private bool started = false;
+	void Start () {
+        speed = startSpeed;
+
+        rb = GetComponent<Rigidbody>();
+	}
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1") && !started) {
+            started = true;
+            rb.AddForce(new Vector3(1, 1) * speed * 5);
+        }
+    }
+}
---
+++ Assets/scripts/PaddleController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+    public float speed = 300;
+    public float clampBetweenMin = -16;
+    public float clampBetweenMax = 16;
+	
+	void Update () {
+        float pos = transform.position.x + Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        pos = Mathf.Clamp(pos, clampBetweenMin, clampBetweenMax);
+
+        transform.position = new Vector3(pos, transform.position.y, transform.position.z);
+	}
+}
