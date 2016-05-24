8b7b7addd0e21d11861e3ef1dd9db42c6e1ce28e
Harrison Shoebridge <harrison@theshoebridges.com>
2016-05-24 21:23:12 +1000

    Add start game screen

---
+++ Assets/scripts/GameStart.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameStart : MonoBehaviour {
+    public void OnStartGame() {
+        SceneManager.LoadScene("game");
+    }
+}
